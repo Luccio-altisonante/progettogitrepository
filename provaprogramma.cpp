f04ed7b3e14e6630fb72c55034859fd4b84857eb
Luca Montanelli <luca05montanelli@gmail.com>
2026-05-21 18:11:31 +0200

    struttura iniziale del progetto

--- provaprogramma.cpp
+++ provaprogramma.cpp
@@ -1,4 +1,11 @@
 #include <iostream>
+
+// struttura dati che dati due nodi del grafo ci dica
+// qual è, se esite, il peso minimo del cammino che collega questi due nodi e quanto è lungo(facoltativo)
+// il peso di un cammino è il peso massimo su un arco di quel cammino
+// io voglio il cammino col peso minimo
+
+// prendere dati da CAIDA
 using namespace std;
 
 int main()
