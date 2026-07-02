0d8c6833a3d95fbef5c5b3dd6bbb72b14f41422c
Luca Montanelli <luca05montanelli@gmail.com>
2026-07-02 12:05:35 +0200

    Aggiunta prima implementazione del secondo pezzo di CostruzioneMST

--- FileHPP/CostruzioneMST.hpp
+++ FileHPP/CostruzioneMST.hpp
@@ -58,4 +58,79 @@ void stampaArchiCCM()
     }
 }
 
+
+//MergeSort visto a lezione, con l'unica modifica che prende un vector<Arcopesato> invece di un vector<int>
+void Fusione(vector<ArcoPesato> &a, int sx, int cx, int dx)
+{
+    int i = sx;
+    int j = cx + 1;
+    int k = 0;
+    vector<ArcoPesato> b(dx - sx + 1);
+
+    while ((i <= cx) && (j <= dx))
+    {
+        if (a[i].peso <= a[j].peso)
+        {
+            b[k] = a[i];
+            i++;
+        }
+        else
+        {
+            b[k] = a[j];
+            j++;
+        }
+
+        k++;
+    }
+
+
+    for ( ; i <= cx; i++, k++)
+    {
+        b[k] = a[i];
+    }
+    for ( ; j <= dx; j++, k++) 
+    {
+        b[k] = a[j];
+    }
+
+
+
+    for (i = sx; i <= dx; i++) 
+    {
+        a[i] = b[i - sx];
+    }
+
+}
+
+
+void MergeSort(vector<ArcoPesato> &a, int sinistra, int destra) 
+{
+    if (sinistra < destra)
+    {
+        int centro = (sinistra + destra) / 2;
+        MergeSort(a, sinistra, centro);
+        MergeSort(a, centro + 1, destra);
+        Fusione(a, sinistra, centro, destra);
+    }
+}
+
+
+void ordinaArchiCCM()
+{
+        MergeSort(archiCCM, 0, archiCCM.size() - 1);
+}
+
+void stampaArchiCCMordinati()
+{
+    cout << endl;
+    cout << "Archi della componente connessa maggiore ordinati:" << endl;
+    cout << endl;
+
+    for (int i = 0; i < archiCCM.size(); i++)
+    {
+        cout << "(" << archiCCM[i].nodo1 << "," << archiCCM[i].nodo2 << ")";
+        cout << " con peso = " << archiCCM[i].peso << endl;
+    }
+}
+
 #endif
