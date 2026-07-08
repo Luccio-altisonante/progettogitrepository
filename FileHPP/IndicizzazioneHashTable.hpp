f080ec2f8e1254828035b27831a5c0cad030409d
Luca Montanelli <luca05montanelli@gmail.com>
2026-07-08 16:23:18 +0200

    Aggiunta prima implementazione del modulo IndicizzazioneHashTable

--- FileHPP/IndicizzazioneHashTable.hpp
+++ FileHPP/IndicizzazioneHashTable.hpp
@@ -13,6 +13,7 @@ struct NodoConIndice
     int nodoReale;
     int indiceCorrisp;
 };
+//il primo elemento sarà il valore del nodo, il secondo l'indice corrispondente
 
 
 class HashUniversale
@@ -41,6 +42,11 @@ public:
         a = rand() % (p - 1) + 1;
         b = rand() % p;
     }
+
+    void stampaParametri()
+    {
+        cout << "a = " << a << ", b = " << b << ", p = " << p << endl;
+    }
 };
 
 
@@ -64,6 +70,7 @@ private:
         return h;
     }
 
+
     void rehash(int nuovoM)
     {
         hasher.rehash();
@@ -81,6 +88,84 @@ private:
         }
         tabella = nuovaTabella;
     }
+
+
+    void inserisciNodo(int x)
+    {    
+        //la funzione viene chiamata da cercaIndice, do per scontato che il nodo non sia già stato inserito
+        NodoConIndice nuovoNodo;
+        nuovoNodo.nodoReale = x;
+        nuovoNodo.indiceCorrisp = indiciToNodi.size();
+        //assegno gli indici per ordine di arrivo:
+        //quando arriva il primo nodo il vector ha dimensione 0 -> va in posizione 0;
+        //quando arriva il secondo nuovo nodo il vector ha dimensione 1 -> va in posizion 1, e coì via.
+
+        indiciToNodi.push_back(x);
+        //così la posizione i del vettore corrisponde al nodo reale di indice i
+
+        int h = hash(x);
+        tabella[h].push_back(nuovoNodo);
+
+        if (tabella[h].size() > 5)
+        {
+            rehash(2 * m + 1);
+        }
+    }
+
+public:
+    HashTable(int size) : m(size), tabella(size), hasher() {}
+
+    int cercaIndice(int nodo)
+    {
+        int h = hash(nodo);
+
+        for (int i = 0; i < tabella[h].size(); i++)
+        {
+            if (tabella[h][i].nodoReale == nodo)
+            {
+                return tabella[h][i].indiceCorrisp;
+            }
+        }
+
+        inserisciNodo(nodo);
+        return cercaIndice(nodo);
+    }
+
+    int cercaNodoReale(int indice)
+    {
+        return indiciToNodi[indice];
+    }
+
+    void stampaHashTable()
+    {   
+        cout << "Struttura finale della hash table:" << endl;
+        cout << endl;
+
+        for (int i = 0; i < tabella.size(); i++)
+        {
+            cout << "Scatola " << i << ": ";
+
+            if (tabella[i].size() == 0)
+            {
+                cout << "vuota";
+            }
+            else
+            {
+                for (int j = 0; j < tabella[i].size(); j++)
+                {
+                    cout << "[nodoReale = " << tabella[i][j].nodoReale
+                         << ", indiceCorrisp = " << tabella[i][j].indiceCorrisp << "] ";
+                }
+            }
+
+            cout << endl;
+        }
+    }
+
+    void stampaParametri()
+    {
+        hasher.stampaParametri();
+    };
 };
 
 #endif
