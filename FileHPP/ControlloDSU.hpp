12fa70c9ac7206438388e286b0531a33472c9afe
Luca Montanelli <luca05montanelli@gmail.com>
2026-07-11 12:04:26 +0200

    Aggiornato modulo dopo le modifiche ai moduli precedenti, rimosse funzione diventate inutili

--- FileHPP/ControlloDSU.hpp
+++ FileHPP/ControlloDSU.hpp
@@ -8,70 +8,50 @@
 
 using namespace std;
 
-struct NodoDSU
-{
-    int nodo;
-    int padre;
-};
-//è la struct che mi permette di fare effettivamente la DSU:
-//per ogni nodo memorizzo chi è il padre, se il padre è uguale al nodo, 
-//allora sarà quello il rappresentante del suo gruppo
 
-vector<NodoDSU> nodiDSU;
-//contiene tutti i nodi della componente connessa maggiore, assieme ai rispettivi padri
+vector<int> padri;
+//grazie all'indicizzazione fatta nei moduli precedenti,
+//padri[i] rappresenta il padre del nodo di indice i.
+//Se padri[i] == i, allora il nodo i è il rappresentante del proprio insieme.
+
 
 void inizializzaDSU()
 {
-    nodiDSU.clear();
-    //necessario perché se la funzione viene chiamata più volte
+    padri.clear();
+    //necessario perché altrimenti se la funzione viene chiamata più volte
     //(come adesso, prima nel main direttamente, poi il main chiama costruisciMST che chiama inizializzaDSU)
     //non crea un vettore nuovo ma continua ad aggiungere gli elementi nuovi al vettore vecchio
-    
-    for (int i = 0; i < componentemaggiore.size(); i++)
-    {
-        NodoDSU nuovonodoDSU;
 
-        nuovonodoDSU.nodo = componentemaggiore[i];
-        nuovonodoDSU.padre = componentemaggiore[i];
+    padri.resize(grafoadj.size());
+    //Si inizializzano tutti i nodi del grafo e non solo quelli della componente maggiore perché questo
+    //semplifica il codice e permette di lavorare sempre sugli stessi indici, rendendo inutile la funzione
+    //trovaPosizioneNodo, e questo non aumenta la complessità finale, perché CostruzioneMST chiamerà le 
+    //funzioni di questo modulo solo sugli archi della componente connessa maggiore.
+
+    for (int i = 0; i < grafoadj.size(); i++)
+    {
+        padri[i] = i;
         //all'inizio sono tutti nodi singoli, quindi ognuno è il proprio rappresentante
-
-        nodiDSU.push_back(nuovonodoDSU);
+        //non è più necessario memorizzare esplicitamente il nodo stesso,
+        //perché la posizione i del vettore rappresenta già il nodo di indice i.
     }
 }
 
+// Grazie all'indicizzazione introdotta nel modulo IndicizzazioneHashTable, il nodo di indice i corrisponde
+// direttamente alla posizione i del vettore padri. Non è quindi più necessaria la funzione per trovare la posizione
+// del nodo all'interno del vettore che nella versione precedente effettuava una ricerca lineare.
 
-
-
-int trovaPosizioneNodo(int nodo)
+int trovaRappresentante(int indiceNodo) 
 {
-    //il nodo i non sarà per forza nella posizione i (o i-1) del vettore nodiDSU,
-    //potrebbe essere ovunque, e dato che mi serve sapere la posizione uso una funzione che la trovi
-    for (int i = 0; i < nodiDSU.size(); i++)
+    if (padri[indiceNodo] == indiceNodo)
     {
-        if (nodiDSU[i].nodo == nodo)
-        {
-            return i;
-        }
-    }
-    return -1;
-
-}
-
-
-
-int trovaRappresentante(int nodo) 
-{
-    int indice = trovaPosizioneNodo(nodo);
-
-    if (nodiDSU[indice].padre == nodo)
-    {
-        return nodo;
+        return indiceNodo;
     }
     //se il padre di un certo nodo è il nodo stesso, dunque allora è lui il rappresentante,
     //quindi,chiamata la funzione trovaRappresentante, posso fare return nodo 
     else 
     {
-        return trovaRappresentante(nodiDSU[indice].padre);
+        return trovaRappresentante(padri[indiceNodo]);
         //se il nodo non è il rappresentante, allora guardo se lo è suo padre, e così via,
         //il rappresentante che trovo sarà il rappresentante del mio nodo di partenza 
         //(e anche degli altri della catena)
@@ -80,32 +60,27 @@ int trovaRappresentante(int nodo)
 }
 
 
-
-
-void unisci(int nodo1, int nodo2)
+void unisci(int indiceNodo1, int indiceNodo2)
 {
-    int rappresentante1 = trovaRappresentante(nodo1);
-    int rappresentante2 = trovaRappresentante(nodo2);
+    int rappresentante1 = trovaRappresentante(indiceNodo1);
+    int rappresentante2 = trovaRappresentante(indiceNodo2);
 
     //se due nodi hanno lo stesso rappresentante vuol dire che appartengono già allo stesso insieme,
     //quindi non devo fare niente
 
     if (rappresentante1 != rappresentante2)
     {
-        int indice = trovaPosizioneNodo(rappresentante2);
-        nodiDSU[indice].padre = rappresentante1;
-        //se i due nodi hanno due rappresentanti diversi, allora devo creare la congiunzione fra i due insiemi,
+        padri[rappresentante2] = rappresentante1;
+        //se i due nodi hanno due rappresentanti diversi, allora si deve creare la congiunzione fra i due insiemi,
         //imponendo che il padre del rappresentante2 diventi rappresentante1, e non più sè stesso.
 
-        // si cambia il padre di rappresentante2 e non di nodo2 perché così tutti gli elementi che avevano
-        // come rappresentante rappresentante2, quindi che appartenevano al secondo insieme, adesso avranno
-        // rappresentante1 come rappresentante, e faranno tutti parte del primo insieme.
-        // altrimenti solo nodo2 ed i suoi figli si sarebbero spostati nel primo insieme,
-        // lasciando tutti gli altri nodi del secondo insieme lì dentro (anche se questo non deve esistere più)
+        //si cambia il padre di rappresentante2 e non di nodo2 perché così tutti gli elementi che avevano
+        //come rappresentante rappresentante2, quindi che appartenevano al secondo insieme, adesso avranno
+        //rappresentante1 come rappresentante, e faranno tutti parte del primo insieme.
+        //altrimenti solo nodo2 ed i suoi figli si sarebbero spostati nel primo insieme,
+        //lasciando tutti gli altri nodi del secondo insieme lì dentro (anche se questo non deve esistere più)
     }
 }
-
-
 
 
 void stampaDSU()
@@ -114,11 +89,11 @@ void stampaDSU()
     cout << "Esito DSU:" << endl;
     cout << endl;
 
-    for (int i = 0; i < nodiDSU.size(); i++)
+    for (int i = 0; i < padri.size(); i++)
     {
-        cout << "Nodo: " << nodiDSU[i].nodo;
+        cout << "Nodo: " << tabella.cercaNodoReale(i);
         cout << ",";
-        cout << " Padre: " << nodiDSU[i].padre << endl;
+        cout << " Padre: " << tabella.cercaNodoReale(padri[i]) << endl;
     }
 }
 
