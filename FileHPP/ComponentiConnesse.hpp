b1c3cb8027bb6a0495f52b0f2140a0f772347270
Luca Montanelli <luca05montanelli@gmail.com>
2026-07-11 11:03:30 +0200

    Aggiornato modulo dopo le modifiche ai moduli precedenti, e modificato modo di trovare la componente connessa maggiore

--- FileHPP/ComponentiConnesse.hpp
+++ FileHPP/ComponentiConnesse.hpp
@@ -8,27 +8,23 @@
 
 using namespace std;
 
-vector<bool> visitato(100, false);
-//vector per sapere quando avviare DFS
-
-
-vector<vector<int>> componenti;
-//ogni elemento di componenti è una componente connessa
+vector<bool> visitato;
+// vector per sapere quando avviare DFS
 
 vector<int> componentemaggiore;
 //conterrà i nodi della componente connessa più grande
 
-void nuovaDFS(int nodo, vector<int> &componentecorrente)
+void nuovaDFS(int indice, vector<int> &componentecorrente)
 {
-    visitato[nodo] = true;
+    visitato[indice] = true;
     //timbra il nodo come già visitato, in modo da non considerarlo più
 
-    componentecorrente.push_back(nodo);
+    componentecorrente.push_back(indice);
     //aggiungo il nodo alla componente corrente
 
-    for (int i = 0; i < grafoadj[nodo].size(); i++)
+    for (int i = 0; i < grafoadj[indice].size(); i++)
     {
-        int vicino = grafoadj[nodo][i];
+        int vicino = grafoadj[indice][i];
 
         if (visitato[vicino] == false)
         {
@@ -44,59 +40,34 @@ void nuovaDFS(int nodo, vector<int> &componentecorrente)
 
 void costruisciComponenti()
 {
+    visitato.resize(grafoadj.size());
+    for (int i = 0; i < visitato.size(); i++)
+    {
+        visitato[i] = false;
+    }
+    //do la giusta dimensione al vettore visitato e lo inizializzo tutto a false
+
     for (int i = 0; i < grafoadj.size(); i++)
     {
-        if (grafoadj[i].size() > 0 && visitato[i] == false)
+        if (visitato[i] == false)
         {
-            //per ora la prima condizione è messa solo perché ho definito arbitrariamente un vettore
-            //lungo 100, quindi ci potrebbero essere delle posizioni di questo vettore vuote, cioè
-            //che non rappresentano nessun elemento del grafo, che hanno 0 vicini, e non voglio considerarle
             vector<int> componentecorrente;
 
             nuovaDFS(i, componentecorrente);
             //avvio la DFS dall'elemento i, se è già stato visitato passerà a uno dei suoi vicini
             //la DFS finisce quando trovo tutta la componente connessa
 
-            componenti.push_back(componentecorrente);
-            //aggiungo la componente al vettore di tutte le componenti
+            if (componentecorrente.size() > componentemaggiore.size())
+            {
+                componentemaggiore = componentecorrente;
+            }
+            //se la nuova componente è maggiore della precedente la salvo, altrimenti la scarto.
+            //Alla fine mi resterà la componente connessa maggiore.
         }
     }
 }
 
-void stampaComponenti()
-{
-    for (int i = 0; i < componenti.size(); i++)
-    {
-        cout << "Componente " << i + 1 << ": ";
 
-        for (int j = 0; j < componenti[i].size(); j++)
-        {
-            cout << componenti[i][j] << " ";
-        }
-
-        cout << endl;
-    }
-}
-
-
-
-
-
-void trovaComponenteMaggiore()
-{
-
-    int indice = 0;
-
-    for (int i = 1; i < componenti.size(); i++)
-    {
-        if (componenti[i].size() > componenti[indice].size())
-        {
-            indice = i;
-        }
-    }
-
-    componentemaggiore = componenti[indice];
-}
 
 void stampaComponenteMaggiore()
 {
@@ -104,7 +75,7 @@ void stampaComponenteMaggiore()
 
     for (int i = 0; i < componentemaggiore.size(); i++)
     {
-        cout << componentemaggiore[i] << " ";
+        cout << tabella.cercaNodoReale(componentemaggiore[i]) << " ";
     }
 
     cout << endl;
