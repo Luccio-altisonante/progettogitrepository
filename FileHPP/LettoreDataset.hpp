ec1ddbc25f6f624e0ab26356d6994d2cb8a1f0c9
Luca Montanelli <luca05montanelli@gmail.com>
2026-07-15 12:23:06 +0200

    modificato LettoreDataset per renderlo adatto a leggere i cammini nel formato in cui si troveranno nei file del dataset CAIDA

--- FileHPP/LettoreDataset.hpp
+++ FileHPP/LettoreDataset.hpp
@@ -11,7 +11,7 @@ using namespace std;
 
 
 vector<vector<int>> cammini; 
-// variabile globale che conterrà tutti i cammini letti dal dataset
+//variabile globale che conterrà tutti i cammini letti dal dataset
 
 
 void leggiDataset(string nomeFile)
@@ -21,39 +21,68 @@ void leggiDataset(string nomeFile)
 
     if(!file.is_open())
     {
-        // Controllo che il file sia stato aperto correttamente
+        //controllo che il file sia stato aperto correttamente
         cout << "Errore nell'apertura del file" << endl;
         return;
     }
 
     string riga;
-    // Variabile che conterrà ogni riga letta dal file
+    //variabile che conterrà ogni riga letta dal file
 
 
 
     while(getline(file, riga))
     {
-        // Continuo finché riesco a leggere righe dal file
+        //continuo finché riesco a leggere righe dal file
         stringstream ss(riga);
-        // Trasformo la riga in uno stream leggibile
+        //trasformo la riga in uno stream leggibile
 
-        vector<int> cammino;
-        // Questo vettore rappresenta il cammino della riga corrente
+        string primaparte;
+        //contiene la prima parte della riga, che non serve al progetto, non verrà utilizzata
 
-        int nodo;
-        // In questa variabile salvo ogni nodo letto
+        string percorso;
+        //contiene il cammino degli Autonomous Systems letto dal file in input
 
-        while(ss >> nodo)
+        ss >> primaparte;
+        //leggo la prima parte della riga
+
+        if (!(ss >> percorso))
         {
-            // Finché riesco a leggere numeri dalla riga li assegno a nodo e li aggiungo al cammino
-            cammino.push_back(nodo);
+            continue;
+            //se la riga è vuota passo alla riga successiva, così da non salvare un nodo 0 inesistente.
         }
 
+        vector<int> cammino;
+        //questo vettore rappresenta il cammino della riga corrente
+
+        int nodo = 0;
+        //variabile che userò per costruire ogni nodo del cammino
+
+        for (char carattere : percorso)
+        //scorro tutto il percorso considerando i caratteri
+        {
+            if (carattere == '|')
+            {
+                //ho terminato di leggere un nodo
+                cammino.push_back(nodo);
+                nodo = 0;
+                //e resetto la variabile nodo per crearne uno nuovo
+            }
+            else
+            {
+                nodo = nodo * 10 + (carattere - '0');
+                //Aggiorno il nodo aggiungendo cifra dopo cifra. Per fare questo sfrutto la
+                //proprietà di consecutività del codice ASCII, per cui 'numero'-'0' restituisce un
+                //intero di valore uguale proprio a numero 
+            }
+        }
+        cammino.push_back(nodo);
+        //inserisco anche l'ultimo nodo del cammino, che non è seguito dal carattere |
 
         if(cammino.size() > 0)
         {
-            // Se il cammino non è vuoto lo aggiungo ai cammini totali
             cammini.push_back(cammino);
+            //se il cammino non è vuoto lo aggiungo ai cammini totali
         }
     }
 
