0088e5ee5af2c1da66462fcfa6c643adfd93f29e
Luca Montanelli <luca05montanelli@gmail.com>
2026-07-16 12:26:20 +0200

    Modifica finale al main per renderlo nella versione definitiva. Rimosse le funzioni di stampa per controllo. Implementata richiesta all'utente per cercare percorso minimax fra due nodi a scelta.

--- FileCPP/main.cpp
+++ FileCPP/main.cpp
@@ -14,6 +14,7 @@ using namespace std;
 
 int main()
 {
+    //scrivere g++ FileCPP/main.cpp -o nomedesiderato nel terminale per avviare. ./nomedesiderato per iniziare. 
     string nomeFile;
 
     cout << "Inserisci il nome del file presente nella cartella FileInput: ";
@@ -21,129 +22,32 @@ int main()
 
     leggiDataset("FileInput/" + nomeFile);
 
-    cout << endl;
-    cout << "Cammini letti dal file:" << endl;
-    cout << endl;
-
-    for (int i = 0; i < cammini.size(); i++)
+    if (cammini.empty())
+    //Se il file non è stato letto potrebbe essere colpa del nome digitato male
     {
-        for (int j = 0; j < cammini[i].size(); j++)
-        {
-            cout << cammini[i][j] << " ";
-        }
-
         cout << endl;
+        cout << "Controllare il nome del file." << endl;
+        return 0;
     }
-    
-    //fin qui prova di LettoreDataset
 
     srand(static_cast<unsigned>(time(nullptr))); // seme iniziale per rand()
 
-    //HashTable tabellaNodi(5); 
-    //per il dataset di prova, la dimensione iniziale è piccola per testare il rehashing 
-
-    //for (int i = 0; i < cammini.size(); i++)
-    //{
-    //    for (int j = 0; j < cammini[i].size(); j++)
-    //    {
-    //        tabellaNodi.cercaIndice(cammini[i][j]);
-    //    }
-    //}
-    //tabellaNodi.stampaParametri();
-    //tabellaNodi.stampaHashTable();
-
-    //fin qui prova di IndicizzazioneHashTable
-
-
-    cout << endl;
-    cout << "Archi costruiti:" << endl;
-    cout << endl;
-
     costruisciArchi();
-
-    stampaArchi();
-
-    cout << endl;
-    cout << "Grafo costruito:" << endl;
-    cout << endl;
-
+    
     costruisciGrafo();
 
-    stampaGrafo();
-
-    //fin qui prova di CostruzioneGrafo
-
-    cout << endl;
-    cout << "Archi pesati:" << endl;
-    cout << endl;
-
     costruisciarchipesati();
-    stampaTabellaArchiPesati();
-    stampaarchipesati();
-    //fin qui prova di GestioneFrequenze
-
-    
 
     costruisciComponenti();
 
-    cout << endl;
-    stampaComponenteMaggiore();
-    cout << endl;
-    //fin qui prova di ComponentiConnesse
-
-
-
-
-    //cout << "Iniziale struttura DSU della componente maggiore: " << endl;
-
-    inizializzaDSU();
-    //stampaDSU();
-    
-
-    //for (int i=0; i < componentemaggiore.size() - 1; i++) 
-    //{
-    //    unisci(componentemaggiore[i],componentemaggiore[i+1]);
-    //    cout << endl << "Struttura DSU della componente maggiore dopo " << i+1 << " unioni:" << endl;
-    //    stampaDSU();
-
-    //}
-    //fin qui prova di ControlloDSU
-
-
-
-
-
-    cout << endl;
-    cout << "Archi della componente connessa maggiore:" << endl;
-    cout << endl;
-
     selezionaArchiCCM();
 
-    stampaArchiCCM();
-
-    cout << endl;
-    cout << "Archi della componente connessa maggiore dopo l'ordinamento:" << endl;
-    cout << endl;
-
     ordinaArchiCCM();
-    stampaArchiCCM();
 
     costruisciMST();
 
-    cout << endl;
-    cout << "Archi dell'MST:" << endl;
-    cout << endl;
-
-    stampaArchiMST();
-
-    //fin qui prova di primo pezzo di CostruzioneMST
-
     costruisciGrafoMST();
-    cout << endl;
-    cout << "Grafo del MST tramite liste di adiacenza: ";
-    cout << endl;
-    stampaGrafoMST();
-
+    
     cout << endl;
     cout << "Esempi di alcuni percorsi minimax con nodi casuali del dataset:" << endl;
     cout << endl;
@@ -171,7 +75,37 @@ int main()
         
     }
 
-    //fin qui prova di RisoluzioneMinimax
+    //ora prendo l'input dall'utente.
+    bool valido = true;
+    while (valido)
+    {
+        int nodo1;
+        int nodo2;
 
-    return 0;
+        cout << "Inserisci due nodi (-1 -1 per terminare): ";
+        cin >> nodo1 >> nodo2;
+
+        if (nodo1 == -1 && nodo2 == -1)
+        {
+            valido = false;
+        }
+        if ((!tabella.ricerca(nodo1)) || (!tabella.ricerca(nodo2)))
+        {
+            cout << "Errore: almeno uno dei due nodi non esiste." << endl;
+            cout << endl;
+            continue;
+        }
+        int minimax = trovaMinimax(nodo1, nodo2);
+
+        if (minimax == -1)
+        {
+            cout << "Errore: almeno uno dei due nodi non appartiene alla componente connessa maggiore." << endl;
+        }
+        else
+        {
+            cout << "Valore minimax: " << minimax << endl;
+        }
+
+        cout << endl;
+    }
 }
